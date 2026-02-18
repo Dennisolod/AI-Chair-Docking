3efccf6a552f374e6d0023bd23ba81934b4c1253
Lucien <lucienackley@gmail.com>
2026-02-18 13:29:13 -0800

    lidar working on rviz, not localized yet though

--- occupancy_map/dummy_controller.py
+++ occupancy_map/dummy_controller.py
@@ -10,16 +10,18 @@ class DummyController(Node):
         self.br = TransformBroadcaster(self)
         self.subscription = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
         
-        # Internal state (x, y, theta)
+        # Internal state (Starting at x=0, y=3)
         self.x = 0.0
         self.y = 3.0
         self.th = 0.0
+        
+        self.vx = 0.0
+        self.vth = 0.0
         
         self.last_time = self.get_clock().now()
         self.timer = self.create_timer(0.1, self.update_pose)
 
     def cmd_vel_callback(self, msg):
-        # We use the velocity commands from Nav2
         self.vx = msg.linear.x
         self.vth = msg.angular.z
 
@@ -47,9 +49,6 @@ class DummyController(Node):
         t.transform.rotation.z = math.sin(self.th / 2.0)
         t.transform.rotation.w = math.cos(self.th / 2.0)
         self.br.sendTransform(t)
-
-    vx = 0.0
-    vth = 0.0
 
 def main():
     rclpy.init()
